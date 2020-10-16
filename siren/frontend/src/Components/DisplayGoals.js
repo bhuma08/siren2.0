4d9778e183e695f19a3a207d33f151b1be759064
bhuma08 <62709043+bhuma08@users.noreply.github.com>
2020-10-16 17:50:32 +0100

    set goals can be viewed!
    NEXT:
    adding new goals

---
+++ siren/frontend/src/Components/DisplayGoals.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+class DisplayGoals extends Component {
+    render() {
+        return (
+            <div>
+                {this.props.goals.map((item, idx) =>
+                    <div key={idx}>
+                        <p>Your Goal: {item.goal}</p>
+                        <p>Actions to help you reach your goal: </p>
+                        <ul>
+                            <li>{item.actionOne}</li>
+                            <li>{item.actionTwo}</li>
+                            <li>{item.actionThree}</li>
+                            <li>{item.actionFour}</li>
+                        </ul>
+                        <p>Start date: {item.start_time}</p>
+                        <p>Deadline: {item.end_time}</p>  <br></br>      
+                    </div>) }
+                
+            </div>
+        )
+    }
+}
+
+export default DisplayGoals
--- siren/frontend/src/Components/Goals.js
+++ siren/frontend/src/Components/Goals.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
 import {connect} from "react-redux";
 import axios from 'axios';
+import DisplayGoals from './DisplayGoals';
 
 class Goals extends Component {
 
+    state= {
+        id : '',
+        goals : []
+    }
+
+    componentDidMount(){
+        const token = this.props.token
+        console.log(token)
+
+        const config = {
+            headers :{
+                'Content-Type': 'application/json',
+                'Authorization' : `Token ${token}`
+            }
+        }
+
+        axios.get('http://127.0.0.1:8000/api/user/', config)
+            .then((response)=> {
+                console.log(response.data.id)
+                this.setState({ id: response.data.id })
+                console.log(this.state.id)
+            })
+            .catch((error)=>{
+                console.log(error);
+            });
+
+
+
+    }
+
     showGoals =()=>{
+
+        console.log(this.state.id)
+
         const token = this.props.token
 
         const config = {
@@ -14,26 +48,33 @@ class Goals extends Component {
             }
         }
 
-        console.log(token)
-
-        axios.get('http://127.0.0.1:8000/api/user/', config)
-            .then(function (response) {
-                console.log(response.data.id);
+        axios.get(`http://127.0.0.1:8000/api/username/${this.state.id}`, config)
+            .then((response)=> {
+                this.setState({ goals : response.data })
+                console.log(this.state.goals)
             })
-              .catch(function (error) {
+            .catch((error)=>{
                 console.log(error);
             });
 
-        
+
+
     };   
 
 
 
 
     render() {
+
+
+
         return (
             <div>
                 <button onClick={this.showGoals}>View your Goals</button>
+                {console.log(this.state.goals)}
+                <DisplayGoals goals={this.state.goals}/>
+
+
                 
             </div>
         )
