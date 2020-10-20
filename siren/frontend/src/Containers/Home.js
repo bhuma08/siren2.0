f0c557f40c6c552453f3415e763c6b0c00e2d969
bhuma08 <62709043+bhuma08@users.noreply.github.com>
2020-10-20 14:20:06 +0100

    added customisation

--- siren/frontend/src/Containers/Home.js
+++ siren/frontend/src/Containers/Home.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react'
 import Logout from '../Components/Logout'
 import Goals from '../Components/Goals'
 import NewGoals from '../Components/NewGoals'
+import {connect} from "react-redux";
 
 class Home extends Component {
+
     render() {
         return (
             <div>
                 <Logout/>
-                <h1>Welcome to your Goals</h1>
+                <h1>Welcome {this.props.username}</h1>
                 <Goals/>
                 <NewGoals/>
                 
@@ -16,5 +18,10 @@ class Home extends Component {
         )
     }
 }
+const mSTP = state =>({
+    
+    username: state.AuthReducer.user.username
+})
 
-export default Home
+
+export default connect(mSTP)(Home);
