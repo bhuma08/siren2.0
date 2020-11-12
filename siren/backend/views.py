e53fc05f66ac64deb34c7912cc3856a695c2f171
bhuma08 <62709043+bhuma08@users.noreply.github.com>
2020-11-12 15:32:01 +0000

    PUT is added
    -habits are able to update: the name of the habit and frequency is a must! So, habit, frequency and progress data is needed to send to backend to update the progress bar!

--- siren/backend/views.py
+++ siren/backend/views.py
@@ -102,6 +102,14 @@ class HabitsDetail(APIView):
         serializer = HabitsSerializer(habits)
         return Response(serializer.data)
 
+    def put(self, request, id, format=None):
+        habits = self.get_object(id)
+        serializer = HabitsSerializer(habits, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     def delete(self, request, id, format=None):
         habits = self.get_object(id)
         habits.delete()
